8556da03d1c309f07fc56335b932bb4e9689d859
VerkhoturovIP <verkhoturovip@sibur.ru>
2023-03-29 17:20:41 +0300

    try deploy #7

--- lib/api.ts
+++ lib/api.ts
@@ -8,6 +8,7 @@ const IS_LOCAL_DEV = process.env.NEXT_PUBLIC_VERCEL_ENV === "development";
 const API_URL = IS_LOCAL_DEV ? `http://localhost:3000` : `https://${VERCEL_URL}`;
 
 
+/*
 console.log("VERCEL_URL:", VERCEL_URL);
 console.log("API_URL:", API_URL);
 
@@ -15,9 +16,11 @@ console.log("ENV:", process.env.NEXT_PUBLIC_VERCEL_ENV ?? "none");
 console.log("URL:", process.env.NEXT_PUBLIC_VERCEL_URL ?? "none");
 
 // console.log("https://yeezyslide-shop-ptmv9s8ud-verkhoturov.vercel.app" === API_URL)
+*/
 
 export const getCatalogList = async (): Promise<CatalogItem[]> => {
   const res = await fetch(`${API_URL}/api/catalog`);
+  console.log("RES:", res, typeof res)
   const data = await res.json();
 
   if (!data || !data.catalogList.length) {
